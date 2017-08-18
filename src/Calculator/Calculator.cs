b44c34180139e181fba60af27e605e200e33660a
Gonzalo <gonzalompp@gmail.com>
2017-08-18 01:03:46 -0300

    Class and method accessibility updated

--- src/Calculator/Calculator.cs
+++ src/Calculator/Calculator.cs
@@ -1,14 +1,14 @@
 
 namespace Calculator
 {
-    class Calculator
+    public class Calculator
     {
 		public static int Add(int num1, int num2 )
 		{
 		return num1 + num2;
 		}
 
-		public int Subtract(int num1, int num2 )
+		public static int Subtract(int num1, int num2 )
 		{
 			if ( num1 > num2 )
 			{
